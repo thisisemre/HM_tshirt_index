2400e745e0cb2fbe7ffc7dd406ae4f95e2fa4c70
Emre Yıldız <emreyildiz183@gmail.com>
2023-04-30 11:34:46 +0300

    Added date to graph title

--- Make_graph.py
+++ Make_graph.py
@@ -1,14 +1,13 @@
 import matplotlib.pyplot as plt
-
+from datetime import datetime
 
 def make_graph(x,y,x_label,y_label,tittle):
-
+    current_date = datetime.now().strftime("%d %b %Y")
+    new_tittle = tittle+" "+current_date
     plt.bar(x, y, color='green', width=0.8)
 
     plt.xlabel(x_label)
-
     plt.ylabel(y_label)
+    plt.title(new_tittle)
 
-    plt.title(tittle)
-
-    plt.savefig(tittle)
+    plt.savefig(new_tittle)
